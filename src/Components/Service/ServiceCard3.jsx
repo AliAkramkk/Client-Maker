bdff6525afe649971b5e15dba970e0311aa511f0
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-20 13:56:54 +0530

    modify contact page and service page

--- src/Components/Service/ServiceCard3.jsx
+++ src/Components/Service/ServiceCard3.jsx
@@ -10,7 +10,7 @@ const ServiceCard3 = () => {
             alt=""
             // src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
             src={ser}
-            className="absolute inset-0 h-full w-full object-cover"
+            className="absolute inset-0 h-full w-full object-cover transition duration-300 ease-in-out transform hover:scale-125"
           />
         </div>
   
--- src/Components/Service/ServiceCard4.jsx
+++ src/Components/Service/ServiceCard4.jsx
@@ -16,7 +16,7 @@ const ServiceCard4 = () => {
           alt=""
       src={ser}
           //   src="https://images.unsplash.com/photo-1496843916299-590492c751f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
-          className="absolute inset-0 h-full w-full object-cover"
+          className="absolute inset-0 h-full w-full object-cover transition duration-300 ease-in-out transform hover:scale-125"
         />
       </div>
 
