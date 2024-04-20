65a986845802fe6eff4f10b27fe821180f6cecad
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-20 19:32:34 +0530

    navbar modified

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -43,10 +43,10 @@ const Navbar = () => {
               
               <Link to="/service">Services</Link>
             </li>
-            <li className="font-montserrat leading-normal text-lg text-white">
+            {/* <li className="font-montserrat leading-normal text-lg text-white">
               
               <Link to="/image">Image</Link>
-            </li>
+            </li> */}
           </ul>
 
           {/* <a href="/" className="flex items-center">
