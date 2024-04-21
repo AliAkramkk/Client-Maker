1400e72d9382d51df21cd0b1097c8b7ed9b058b6
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-21 12:09:10 +0530

    home page background color change

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
       "linear-gradient(to right,  rgb(250, 224, 226), rgb(189, 193, 199))",
   };
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-900">
       <Navbar />
       <section
         className="relative bg-gray-900 bg-cover bg-center bg-no-repeat"
@@ -84,139 +84,9 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <h1 className="text-4xl font-sans text-gray-800 mb-4 mt-8 text-center">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4 mt-8 text-center">
               Our Services </h1>
-    {/* <Slider {...settings}>
-      <div className="flex justify-center">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-80 bg-gray-200 mx-2 rounded-md my-4 overflow-hidden hover:bg-gray-300 ">
-                  <div className=" pt-2">
-      <article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-    className="absolute inset-0 h-full w-full object-cover"
-  />
-
-  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
-      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
-
-      <a href="#">
-        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-      </a>
-
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-        atque dignissimos. Molestias explicabo corporis voluptatem?
-      </p>
-    </div>
-  </div>
-</article>
-
-<article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-    className="absolute inset-0 h-full w-full object-cover"
-  />
-
-  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
-      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
-
-      <a href="#">
-        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-      </a>
-
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-        atque dignissimos. Molestias explicabo corporis voluptatem?
-      </p>
-    </div>
-  </div>
-</article>
-
-<article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-    className="absolute inset-0 h-full w-full object-cover"
-  />
-
-  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
-      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
-
-      <a href="#">
-        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-      </a>
-
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-        atque dignissimos. Molestias explicabo corporis voluptatem?
-      </p>
-    </div>
-  </div>
-</article>
-
-<article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-    className="absolute inset-0 h-full w-full object-cover"
-  />
-
-  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
-      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
-
-      <a href="#">
-        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-      </a>
-
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-        atque dignissimos. Molestias explicabo corporis voluptatem?
-      </p>
-    </div>
-  </div>
-</article>
-<article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-    className="absolute inset-0 h-full w-full object-cover"
-  />
-
-  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
-      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
-
-      <a href="#">
-        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-      </a>
-
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-        atque dignissimos. Molestias explicabo corporis voluptatem?
-      </p>
-    </div>
-  </div>
-</article>
-      </div>
-      </div>
-      </div>
-    </Slider> */}
+    
 
 <Slider {...settings}>
 
