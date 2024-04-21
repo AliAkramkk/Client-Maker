319e246913fa7f46e76e111ec1486f5e0a610d01
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-21 23:45:51 +0530

    service page modified

--- src/Pages/Service1.jsx
+++ src/Pages/Service1.jsx
@@ -4,15 +4,12 @@ import Navbar from '../Components/Navbar/Navbar';
 const Service1 = () => {
     const backgroundColorStyle = {
         backgroundColor: `rgb(234, 239, 177)`,
-        display: 'flex',
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    height: '100vh'
+       
       };
   return (
     <div className=''  style={backgroundColorStyle}>
         <Navbar />
-        <div className='bg-blue-50 m-4 mt-2 w-96 h-36 ' style={{ width: '70%' }}>item1</div>
+        <div className='bg-blue-50 m-4 mt-2 w-96 h-36 flex justify-items-center ' style={{ width: '70%' }}>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
