01b6dbc459790dadd8f7265136e01d9d9b03c246
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-21 12:37:41 +0530

    home page section card2 modified

--- src/Components/Home/SectionCard.jsx
+++ src/Components/Home/SectionCard.jsx
@@ -23,7 +23,7 @@ const SectionCard = () => {
           </p> */}
         </div>
 
-        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 bg-gray-900">
           <a
             className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
             href="#"
