575536c10dd9ed7b01a28b70338ec16db4b749f9
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-21 23:39:56 +0530

    service page modified

--- src/Pages/Service1.jsx
+++ src/Pages/Service1.jsx
@@ -6,7 +6,7 @@ const Service1 = () => {
       };
   return (
     <div className=''  style={backgroundColorStyle}>
-        <div className='bg-blue-100 m-4 mt-2 w-80 h-40'>item1</div>
+        <div className='bg-blue-50 m-4 mt-2 w-96 h-36' style={{ width: '70%' }}>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
         <div className='bg-blue-100 m-4'>item1</div>
