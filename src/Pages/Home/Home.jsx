e08bd6e77024bcaff4fbb505cd4a606811addd17
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-21 12:31:30 +0530

    home page service image modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,10 @@ import Navbar from "../../Components/Navbar/Navbar";
 import SectionCard from "../../Components/Home/SectionCard";
 import uaeCard from "../../assets/homeMain.jpg";
 import SectionCard2 from "../../Components/Home/SectionCard2";
+import ser from  '../../assets/marketingMaker.jpg'
+import ser1 from '../../assets/maker.jpg'
+import ser2 from  '../../assets/co.jpg'
+import ser3 from '../../assets/webdevelopment.jpg'
 import Footer from "../../Components/Footer/Footer";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -95,7 +99,7 @@ const Home = () => {
       <article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
   <img
     alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
+    src={ser}
     className="absolute inset-0 h-full w-full object-cover"
   />
 
@@ -121,7 +125,7 @@ const Home = () => {
       <article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
   <img
     alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
+    src={ser1}
     className="absolute inset-0 h-full w-full object-cover"
   />
 
@@ -147,7 +151,33 @@ const Home = () => {
       <article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
   <img
     alt=""
-    src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
+    src={ser2}
+    className="absolute inset-0 h-full w-full object-cover"
+  />
+
+  <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+    <div className="p-4 sm:p-6">
+      <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
+
+      <a href="#">
+        <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
+      </a>
+
+      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+        pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+        quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
+        atque dignissimos. Molestias explicabo corporis voluptatem?
+      </p>
+    </div>
+  </div>
+</article>
+      </div>
+      <div className="p-4">
+      <article className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+  <img
+    alt=""
+    src={ser3}
     className="absolute inset-0 h-full w-full object-cover"
   />
 
