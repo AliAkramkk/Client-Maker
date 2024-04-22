07dfabbc136059b52948f7e18a53e30ffed88793
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-22 11:46:29 +0530

    home page  modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
         className="relative bg-contain bg-center bg-no-repeat h-screen"
         style={{ backgroundImage: `url(${uaeCard})`,
         backgroundSize: 'contain', 
-        backgroundPosition: 'right center', 
+        backgroundPosition: '95% center', 
         backgroundRepeat: 'no-repeat' ,
         
       }}
