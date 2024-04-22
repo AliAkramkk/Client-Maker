ec8f307fdbf124e7a439f296beb90fb937464311
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-22 15:33:23 +0530

    home page section card2  modified

--- src/Components/Home/SectionCard2.jsx
+++ src/Components/Home/SectionCard2.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import section from "../../assets/boy.jpeg";
+import section from "../../assets/boy1.png";
+
 const SectionCard2 = () => {
   return (
     <section>
@@ -11,13 +12,13 @@ const SectionCard2 = () => {
                 alt=""
                
                 src={section}
-                className="absolute inset-0 h-full w-full object-cover"
+                className="absolute inset-0 h-full w-full object-contain"
               />
             </div>
           </div>
 
-          <div className="relative flex items-center bg-gray-100">
-            <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"></span>
+          <div className="relative flex items-center bg-black">
+            <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-black"></span>
 
             <div className="p-8 sm:p-16 lg:p-24">
               {/* <h2 className="text-2xl font-bold sm:text-3xl">
@@ -25,16 +26,16 @@ const SectionCard2 = () => {
                 Tempore, debitis.
               </h2> */}
 
-              <p className=" text-black font-medium">
+              <p className=" text-white font-medium">
               Hey ,<br />
 Welcome to TheClientsMaker.com, a leading digital marketing and business development agency nestled in the vibrant landscape of Dubai. We specialize in crafting compelling digital experiences tailored to elevate your brand's online presence and drive tangible results.
 
               </p>
-              <p className=" text-black font-medium">At TheClientsMaker.com, we understand that in today's dynamic digital world, having a strong online presence is paramount. That's why we offer a comprehensive suite of services designed to empower businesses to thrive in the digital realm. From stunning website design and captivating video production to engaging photography and expertly crafted content, we're your one-stop destination for all things digital marketing.
+              <p className=" text-white font-medium">At TheClientsMaker.com, we understand that in today's dynamic digital world, having a strong online presence is paramount. That's why we offer a comprehensive suite of services designed to empower businesses to thrive in the digital realm. From stunning website design and captivating video production to engaging photography and expertly crafted content, we're your one-stop destination for all things digital marketing.
 </p>
-<p className=" text-black font-medium">With a team of seasoned professionals boasting a wealth of experience and a deep understanding of the ever-evolving digital landscape, we pride ourselves on delivering innovative solutions that exceed expectations. Our relentless commitment to excellence, coupled with our passion for creativity, sets us apart as leaders in the industry.
+{/* <p className=" text-black font-medium">With a team of seasoned professionals boasting a wealth of experience and a deep understanding of the ever-evolving digital landscape, we pride ourselves on delivering innovative solutions that exceed expectations. Our relentless commitment to excellence, coupled with our passion for creativity, sets us apart as leaders in the industry.
 
-</p>
+</p> */}
 {/* <p className=" text-black font-medium">
 But we're not just about creating impressive digital assets; we're also dedicated to driving meaningful business growth for our clients. Through strategic business development initiatives, we help businesses unlock their full potential and achieve sustainable success in today's competitive market.
 </p>
