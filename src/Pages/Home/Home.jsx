80cc26440613c0f32f7a46271bbfc93cfb7803f3
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-22 11:08:24 +0530

    home page  modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -60,10 +60,10 @@ const Home = () => {
         <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
           <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
             <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
-            Opening a new era  
+            Opening a new era for 
               <strong className="block font-extrabold "> 
               
-              for your Business
+               your Business
                 
                  <span className=" text-rose-700 font-serif text-7xl animate-spin ... shadow-slate-950">
                "GROWTH"
