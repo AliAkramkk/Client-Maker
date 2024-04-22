48e0b542356f1dfea5f9bac7f60bbe81eeaacbd9
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-22 20:09:37 +0530

    quates modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -86,6 +86,7 @@ const Home = () => {
       </div>
     </div>
   </section>
+        <Quotes />
       <SectionCard2 />
       <h1 className="text-4xl font-bold text-gray-800 mb-4 mt-8 text-center">
               Our Services </h1>
@@ -221,7 +222,6 @@ Boost your online visibility with expert SEO services from TheClientsMaker.com. 
      
       <SectionCard />
 
-      <Quotes />
       {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.1" d="M0,288L60,277.3C120,267,240,245,360,213.3C480,181,600,139,720,149.3C840,160,960,224,1080,224C1200,224,1320,160,1380,128L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg> */}
       <Footer />
     </div>
