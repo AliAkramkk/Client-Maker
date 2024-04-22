bdb7877f13da1b87f61d541ff3e5b7873e9092f3
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-22 20:36:45 +0530

    quates modified

--- src/Pages/Quotes.jsx
+++ src/Pages/Quotes.jsx
@@ -13,9 +13,9 @@ const Quotes = () => {
       ];
     
       return (
-        <Marquee autoFill pauseOnHover speed={20}>
+        <Marquee autoFill pauseOnHover speed={10}>
           {quotes.map((quote, index) => (
-            <span key={index} className='text-white'>{quote} </span>
+            <span key={index} className='text-base font-medium m-2 px-2.5 py-2.5 text-white'>{quote} </span>
           ))}
         </Marquee>
       );
