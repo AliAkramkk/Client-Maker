e6e1b8133c657f9827fe9d4a14bf80ca304bff09
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-24 12:15:12 +0530

    home page modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -58,14 +58,14 @@ const Home = () => {
     <div className="bg-black">
       <Navbar />
       <section
-        className="relative bg-contain bg-center bg-no-repeat min-h-screen"
+        className="relative bg-contain bg-center bg-no-repeat "
         style={{
           backgroundImage: `url(${uaeCard})`,
           backgroundSize: "contain",
           backgroundPosition: "85% 15%",
           backgroundRepeat: "no-repeat",
-          padding: "0 5px", // Adjust padding to create space between content and image
-          maxWidth: "100vw", // Ensure the section doesn't exceed the viewport width
+          padding: "0 5px",
+          maxWidth: "100vw", 
         }}
       >
         <div className="absolute inset-0 bg-black/75 sm:bg-transparent sm:from-black/95 sm:to-black/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
