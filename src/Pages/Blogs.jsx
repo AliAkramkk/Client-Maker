2b84c5f5480b2ca449cd7fd7628dd76038e2251e
AliAkramkk <akramkorakkottil@gmail.com>
2024-04-25 17:04:31 +0530

    whatsapp button create and all file modified

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -9,7 +9,7 @@ const Blogs = () => {
     useEffect(() => {
         const interval = setInterval(() => {
           nextSlide();
-        }, 3000); // Adjust the interval duration as needed
+        }, 3000); 
     
         return () => clearInterval(interval);
       }, []);
@@ -117,26 +117,7 @@ const Blogs = () => {
                     <img
                       alt=""
                       src={ser}
-                      className="absolute inset-0 h-full w-full object-cover transition duration-300 ease-in-out transform hover:scale-105"
-                    />
-                    <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-                      <div className="p-4 sm:p-6">
-                        <h2 className="mt-0.5 text-lg text-white">Digital Marketing</h2>
-                        <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-                          Unlock your business's digital potential with TheClientsMaker.com. We specialize in crafting tailored digital strategies to elevate your brand and drive results. From captivating website design to engaging content creation, we offer a comprehensive suite of digital marketing services. Let us help you thrive in the online landscape.
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                </Link>
-              </div>
-              <div className="p-4">
-                <Link to="/">
-                  <article className="relative overflow-hidden rounded-lg shadow transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg hover:shadow-sky-400">
-                    <img
-                      alt=""
-                      src={ser2}
-                      className="absolute inset-0 h-full w-full object-cover transition duration-300 ease-in-out transform hover:scale-105"
+                      className="absolute inset-0 h-full w-full object-contain transition duration-300 ease-in-out transform hover:scale-105"
                     />
                     <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
                       <div className="p-4 sm:p-6">
