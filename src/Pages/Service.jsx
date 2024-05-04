971350d7ff8ab698bf89a69f052dec5bb0389501
AliAkramkk <akramkorakkottil@gmail.com>
2024-05-04 21:15:17 +0530

    service page modified add automatic service in the herosection

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar/Navbar'
 import logo from '../assets/new3.jpg'
 import ServiceCard1 from '../Components/Service/ServiceCard1'
@@ -10,6 +10,25 @@ import ServiceCard5 from '../Components/Service/ServiceCard5'
 import './Service1.css'
 import WhatsappButton from '../Components/WhatsappButton'
 const Service = () => {
+
+  const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
+
+  const services = [
+    'Content Making',
+    'Google Ads',
+    'Web Application',
+    'Instagram Marketing'
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentServiceIndex((prevIndex) =>
+        prevIndex === services.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); 
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className='bg-black'>
       <Navbar />
@@ -21,10 +40,12 @@ const Service = () => {
             className="mb-4 max-w-xs md:mr-6 md:mb-0"
           />
           <h3 className="text-xl md:text-2xl lg:text-4xl text-center md:text-left jerking-effect">
-            <strong className='text-white'>your digital salesman </strong>
-            <span className='text-red-700 font-bold'></span>
-            <br />
-            <span className='text-base font-sans'></span>
+            <strong className='text-white'>Our services are : </strong><br/>
+            <span className="text-red-700 font-bold">
+              {services[currentServiceIndex]}
+            </span>
+           
+            
           </h3>
         </div>
         </div>
